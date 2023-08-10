329f4fa3402b8940e3f6689a16b6ba36c492c96f
washingtonwatitwa <washingtonwatitwa@gmail.com>
2023-08-10 16:45:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: success if value is zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
