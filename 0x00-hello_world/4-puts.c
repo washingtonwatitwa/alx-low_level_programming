a416c34db53cd08cef372ab75c2df63dc809f772
washingtonwatitwa <washingtonwatitwa@gmail.com>
2023-08-10 16:30:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main starting point
- * return - always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
